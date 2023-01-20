25d05cdf7fdd3d921d1d03380b8ae8d5f1fd03e5
Buzzuzz <stefunkovolodymyr@gmail.com>
2023-01-21 01:28:50 +0200

    user commands refactor

--- src/main/java/com/my/project/controller/commands/impl/user/ChangeUserLockStatusCommand.java
+++ src/main/java/com/my/project/controller/commands/impl/user/ChangeUserLockStatusCommand.java
@@ -1,6 +1,5 @@
 package com.my.project.controller.commands.impl.user;
 
-import com.my.project.constants.AttributeConstants;
 import com.my.project.constants.CommandNameConstants;
 import com.my.project.controller.commands.Command;
 import com.my.project.exceptions.CommandException;
@@ -10,27 +9,29 @@ import lombok.extern.log4j.Log4j2;
 import com.my.project.services.impl.UserServiceImpl;
 import com.my.project.utils.RequestBuilder;
 
+import static com.my.project.constants.AttributeConstants.*;
+
 @Log4j2
 public class ChangeUserLockStatusCommand implements Command {
     @Override
     public String execute(HttpServletRequest req) throws CommandException {
         try {
             UserServiceImpl.getInstance().changeUserLockStatus(
-                    Long.parseLong(req.getParameter(AttributeConstants.USER_ID)),
-                    !Boolean.parseBoolean(req.getParameter(AttributeConstants.USER_STATUS)));
+                    Long.parseLong(req.getParameter(USER_ID)),
+                    !Boolean.parseBoolean(req.getParameter(USER_STATUS)));
             return RequestBuilder.buildCommand(
                     req.getServletPath(),
                     CommandNameConstants.GET_ALL_USERS_COMMAND,
                     RequestBuilder.getSpecifiedParamsMap(
                             req.getParameterMap(),
-                            AttributeConstants.SORTING_TYPE,
-                            AttributeConstants.DISPLAY_RECORDS_NUMBER,
-                            AttributeConstants.CURRENT_PAGE,
-                            AttributeConstants.ERROR_ATTR,
-                            AttributeConstants.USER_TYPE_DB));
+                            SORTING_TYPE,
+                            DISPLAY_RECORDS_NUMBER,
+                            CURRENT_PAGE,
+                            ERROR_ATTR,
+                            USER_TYPE_DB));
         } catch (ServiceException e) {
-            log.error("Can't change user(id) status: " + req.getParameter(AttributeConstants.USER_ID), e);
-            throw new CommandException("Can't change user(id) status: " + req.getParameter(AttributeConstants.USER_ID), e);
+            log.error("Can't change user(id) status: " + req.getParameter(USER_ID), e);
+            throw new CommandException("Can't change user(id) status: " + req.getParameter(USER_ID), e);
         }
     }
 }
--- src/main/java/com/my/project/controller/commands/impl/user/DeleteUserCommand.java
+++ src/main/java/com/my/project/controller/commands/impl/user/DeleteUserCommand.java
@@ -9,22 +9,24 @@ import lombok.extern.log4j.Log4j2;
 import com.my.project.services.impl.UserServiceImpl;
 import com.my.project.utils.RequestBuilder;
 
+import static com.my.project.constants.AttributeConstants.*;
+
 @Log4j2
 public class DeleteUserCommand implements Command {
     @Override
     public String execute(HttpServletRequest req) throws CommandException {
         try {
-            UserServiceImpl.getInstance().deleteUser(Long.parseLong(req.getParameter(AttributeConstants.USER_ID)));
+            UserServiceImpl.getInstance().deleteUser(Long.parseLong(req.getParameter(USER_ID)));
             return RequestBuilder.buildCommand(
                     req.getServletPath(),
                     CommandNameConstants.GET_ALL_USERS_COMMAND,
                     RequestBuilder.getSpecifiedParamsMap(
                             req.getParameterMap(),
-                            AttributeConstants.SORTING_TYPE,
-                            AttributeConstants.DISPLAY_RECORDS_NUMBER,
-                            AttributeConstants.CURRENT_PAGE,
-                            AttributeConstants.ERROR_ATTR,
-                            AttributeConstants.USER_TYPE_DB));
+                            SORTING_TYPE,
+                            DISPLAY_RECORDS_NUMBER,
+                            CURRENT_PAGE,
+                            ERROR_ATTR,
+                            USER_TYPE_DB));
         } catch (Exception e) {
             log.error("Can't delete user", e);
             throw new CommandException("Can't execute delete-user command", e);
